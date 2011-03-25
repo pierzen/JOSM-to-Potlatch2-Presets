e21b21a692a910af7e699c47ae84832ee36db958
Pierre Beland <infosbelas-am@yahoo.fr>
2011-03-24 21:04:52 -0400

    GNU General Public License version 3

--- josm-to-potlach2-preset-conversion.py
+++ josm-to-potlach2-preset-conversion.py
@@ -13,6 +13,18 @@ print "=========================================================================
  potlatch2_preset : potlatch2 preset file (output)
  OUTPUT to potlatch2 group menu file
  potlatch2_group : potlatch2 group file (output) : creates a file for each first level of group tag
+ 
+ Copyright (c) 2011 Pierre Beland
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =========================================================================
 """
 
